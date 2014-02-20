c90139ac357720b40f0d9c22120b09f9ec2d4092
MartoniczD <registration@martonicz.com>
2014-02-20 19:43:10 +0100

    non working dma

--- Sonar/ComplexDrivers/CD_AnalogIn.c
+++ Sonar/ComplexDrivers/CD_AnalogIn.c
@@ -1,9 +1,82 @@
+/* @file    CD_AnalogIn.c
+ * @author  S&I Application Team
+ * @version V1.0.0
+ * @date    16-February-2014
+ * @brief   This file provides firmware functions to manage the Analog Input part of the ECU.
+ *           + Initialization and Configuration
+ *           + Measurement
+ */
+
+
 #include "CD_AnalogIn.h"
 
 
-enum { ADC_HALF_BUFFER = 18 };
-volatile uint16 ADCbuf[ADC_HALF_BUFFER];
+enum { 
+	
+	ADC_BUFFER_SIZE		 		= 36,
+	ADC_HALF_BUFFER_SIZE	= 18
 
+};
+volatile uint16 ADCbuf[ADC_BUFFER_SIZE];
+
+/**
+ * @section Local functions
+ */
+static void setup_adc_gpio(void);
+static void setup_adc_dma(void);
+static void calibrate_adc(void);
+static void setup_adc(void);
+/**
+ *
+ *
+ */
+
+
+
+/** @addtogroup Sonar_Application
+  * @brief Main application container
+  * @{
+  */
+
+
+/** @addtogroup ComplexDrivers
+  * @brief Drivers of mcu c. peripherals.
+  * @{
+  */
+
+/** @addtogroup Analog_Input
+  * @brief Drivers of the ADC interface.
+  * @details Initializes the ADC/DMA interface.
+  * @{
+  */
+
+
+/**
+ * @name CD_AnalogIn_Init(void)
+ * @brief Initializes the analog output.
+ * @param None
+ * @return None
+ * @callergraph
+ */
+void CD_AnalogIn_Init(void){
+
+	setup_adc();
+
+	return;
+}
+
+
+/*
+ * @}
+ */
+
+/*
+ * @}
+ */
+
+/*
+ * @}
+ */
 
 static void setup_adc_gpio(void)
 {
@@ -13,10 +86,10 @@ static void setup_adc_gpio(void)
 
 	/* Configure ADC1/2 channels as analog input */
 	GPIO_StructInit(&GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_4;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+	GPIO_InitStructure.GPIO_Speed 	= GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Pin 	= GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode 	= GPIO_Mode_AN;
+	GPIO_InitStructure.GPIO_PuPd 	= GPIO_PuPd_NOPULL ;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
@@ -24,9 +97,9 @@ static void setup_adc_dma(void)
 {
 	DMA_InitTypeDef        DMA_InitStructure;
 	/* Enable DMA1 clock */
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA2, ENABLE);
 
-	DMA_DeInit(DMA1_Channel1);	
+	DMA_DeInit(DMA2_Channel1);
 	
 	/* DMA1 Channel1 Init */
 	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1_2->CDR;
@@ -41,13 +114,13 @@ static void setup_adc_dma(void)
 	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
 	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
 
-	DMA_Init(DMA1_Channel1, &DMA_InitStructure);
+	DMA_Init(DMA2_Channel1, &DMA_InitStructure);
 
 	/* DMA interrupts */
-	DMA_ITConfig(DMA1_Channel1, DMA_IT_TC | DMA_IT_HT, ENABLE);
+	DMA_ITConfig(DMA2_Channel1, DMA_IT_TC | DMA_IT_HT, ENABLE);
 
 	/* enable DMA IRQ */
-	NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+	NVIC_EnableIRQ(DMA2_Channel1_IRQn);
 } 
 
 static void calibrate_adc(void)
@@ -56,32 +129,32 @@ static void calibrate_adc(void)
 //	__IO uint16_t calibration_value_2 = 0;	
 
   /* ADC Calibration procedure */
-  ADC_VoltageRegulatorCmd(ADC1, ENABLE);
+  ADC_VoltageRegulatorCmd(ADC2, ENABLE);
  // ADC_VoltageRegulatorCmd(ADC2, ENABLE);
   
   /* Insert delay equal to 10 µs */
   Delay_us(10);
   
-  ADC_SelectCalibrationMode(ADC1, ADC_CalibrationMode_Single);
-  ADC_StartCalibration(ADC1);
+  ADC_SelectCalibrationMode(ADC2, ADC_CalibrationMode_Single);
+  ADC_StartCalibration(ADC2);
 
 //  ADC_SelectCalibrationMode(ADC2, ADC_CalibrationMode_Single);
 //  ADC_StartCalibration(ADC2);
   
-  while(ADC_GetCalibrationStatus(ADC1) != RESET );
-  calibration_value_1 = ADC_GetCalibrationValue(ADC1);
+  while(ADC_GetCalibrationStatus(ADC2) != RESET );
+  calibration_value_1 = ADC_GetCalibrationValue(ADC2);
 
 //  while(ADC_GetCalibrationStatus(ADC2) != RESET );
 //  calibration_value_2 = ADC_GetCalibrationValue(ADC2);	
 }
 
-void setup_adc(void)
+static void setup_adc(void)
 {
 	ADC_CommonInitTypeDef	 ADC_CommonInitStructure;
 	ADC_InitTypeDef        ADC_InitStructure;
 		
 	
-	ADC_DeInit(ADC1);
+	//ADC_DeInit(ADC1);
 	ADC_DeInit(ADC2);
 	
 	/* Configure the ADC clock */
@@ -101,7 +174,7 @@ void setup_adc(void)
 	ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_1 ; // ADC_DMAAccessMode_2
 	ADC_CommonInitStructure.ADC_DMAMode = ADC_DMAMode_Circular; 
 	ADC_CommonInitStructure.ADC_TwoSamplingDelay = 0; 
-	ADC_CommonInit(ADC1, &ADC_CommonInitStructure);
+	ADC_CommonInit(ADC2, &ADC_CommonInitStructure);
 
 	/* ADC setup */  
 	ADC_StructInit(&ADC_InitStructure);
@@ -113,58 +186,58 @@ void setup_adc(void)
 	ADC_InitStructure.ADC_OverrunMode = ADC_OverrunMode_Disable;   
 	ADC_InitStructure.ADC_AutoInjMode = ADC_AutoInjec_Disable;  
 	ADC_InitStructure.ADC_NbrOfRegChannel = 1;
-	ADC_Init(ADC1, &ADC_InitStructure);
+	ADC_Init(ADC2, &ADC_InitStructure);
 	//ADC_Init(ADC2, &ADC_InitStructure);
 
 	// PA0 = ADC1 IN1 (channel 1)
-  ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_19Cycles5);
+  ADC_RegularChannelConfig(ADC2, ADC_Channel_2, 1, ADC_SampleTime_19Cycles5);
 	// PA4 = ADC2 IN1 (channel 1)
   //ADC_RegularChannelConfig(ADC2, ADC_Channel_1, 1, ADC_SampleTime_19Cycles5);	
 	
 	/* Configures the ADC DMA */
-	ADC_DMAConfig(ADC1, ADC_DMAMode_Circular);
+	ADC_DMAConfig(ADC2, ADC_DMAMode_Circular);
 		
 	/* Enable the ADC DMA */
-	ADC_DMACmd(ADC1, ENABLE);
+	ADC_DMACmd(ADC2, ENABLE);
 
   /* Enable ADC1 and ADC2 */
-  ADC_Cmd(ADC1, ENABLE);
+  ADC_Cmd(ADC2, ENABLE);
   //ADC_Cmd(ADC2, ENABLE);
 
 	/* wait for ADC1 ADRDY */
-	while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_RDY));
+	while(!ADC_GetFlagStatus(ADC2, ADC_FLAG_RDY));
 	
   /* wait for ADC2 ADRDY */
   //while(!ADC_GetFlagStatus(ADC2, ADC_FLAG_RDY));	
 
 	/* Enable the DMA channel */
-	DMA_Cmd(DMA1_Channel1, ENABLE);
+	DMA_Cmd(DMA2_Channel1, ENABLE);
 
 	/* Start ADC1 Software Conversion */ 
-	ADC_StartConversion(ADC1);
+	ADC_StartConversion(ADC2);
 }
 
 /* Handles DMA1_Channel1 (ADC Buffer) interrupt request */
 // void DMA1_Channel1_IRQHandler(void) __attribute__ ((section (".ccmram")));
-// void DMA1_Channel1_IRQHandler(void)
-// {
-// 	unsigned int *ptr;
-// 	
-// 	if(DMA1->ISR & DMA1_IT_HT1)
-// 	{
-// 		/* Half transfer - 1st half buffer has data */
-// 		ptr = &ADCbuf[0];
-// 	}
-// 	else
-// 	{
-// 		/* Transfer complete - 2nd half buffer has data */
-// 		ptr = &ADCbuf[ADC_HALF_BUFFER];
-// 	}
+ void DMA2_Channel1_IRQHandler(void)
+ {
+ 	volatile uint16 *ptr;
 
-// 	/* process the buffer */
-// 	adc_data_callback(ptr, ADC_HALF_BUFFER);		
-// 	
-// 	/* Clear DMA1_Channel1 interrupt */
-// 	DMA1->IFCR = DMA_IFCR_CGIF1;
-// }
+ 	if(DMA2->ISR & DMA2_IT_HT1)
+ 	{
+ 		/* Half transfer - 1st half buffer has data */
+ 		ptr = &ADCbuf[0];
+ 	}
+ 	else
+ 	{
+ 		/* Transfer complete - 2nd half buffer has data */
+ 		ptr = &ADCbuf[ADC_HALF_BUFFER_SIZE];
+ 	}
+
+ 	/* process the buffer */
+// 	adc_data_callback(ptr, ADC_HALF_BUFFER);
+
+ 	/* Clear DMA1_Channel1 interrupt */
+ 	DMA2->IFCR = DMA_IFCR_CGIF1;
+ }
 
